9b48919e119190adc679b30b467670aca4ffece6
SZUCRAFT <SZUCRAFT@163.com>
2024-11-10 00:37:56 +0800

    增加第一次任务说明

---
+++ Bar Render/Demo/src/main/java/io/xyz/bar_render/BarRender.java
@@ -0,0 +1,10 @@
+package io.xyz.bar_render;
+
+import net.fabricmc.api.ModInitializer;
+
+public class BarRender implements ModInitializer {
+    @Override
+    public void onInitialize() {
+
+    }
+}
---
+++ Bar Render/Demo/src/main/java/io/xyz/bar_render/client/BarRenderClient.java
@@ -0,0 +1,10 @@
+package io.xyz.bar_render.client;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class BarRenderClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
